6b3766bd441cddd4c0dd193a207f0e2f2c087422
Akhilesh98 <akhilesh98moonat@gmail.com>
2016-09-18 02:57:05 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function (req, res) {
     res.sendFile('Article one is requested and will be served here');
-});    
+});   
+
 app.get('/article-two', function (req, res) {
     res.sendFile('Article two is requested and will be served here');
 });
+
 app.get('/article-three', function (req, res) {
     res.sendFile('Article three is requested and will be served here');
 });
